f6908756a16253e8caa6e5c974f16efacb67b6bf
211250116-Cai Haorun <211250116@smail.nju.edu.cn>
2024-03-13 14:50:42 +0800

    updated logs

--- run.ps1
+++ run.ps1
@@ -1,17 +1,18 @@
-$dataset = 'Laptop_Prices_Dataset', '../communities_and_crime', '../cpu_small', 'water_quality', '../ada_agnostic', '../Basketball_c', '../c131', '../UJIndoorLoc'
+# $dataset = 'Laptop_Prices_Dataset', '../communities_and_crime', '../cpu_small', 'water_quality', '../ada_agnostic', '../Basketball_c', '../c131', '../UJIndoorLoc'
+$dataset = 'water_quality', '../ada_agnostic', '../Basketball_c', '../c131'
 $tf = "False", "True"
 
-# foreach ($data in $dataset) {
-#     # Add-Content -Path "log.txt" -Value "======== dataset: $data ========`n"
-#     # foreach ($rf in $tf) {
-#     #     foreach ($bias in $tf) {
-#             # Add-Content -Path "log.txt" -Value "---- rf $rf - bias $bias ----`n"
-#             # D:/anaconda3/envs/pytorch/python.exe ./example.py --dataset $data
-#             D:/anaconda3/envs/pytorch/python.exe ./main.py --dataset $data --pretrain True
-#     #     }
-#     # }
-#     # Add-Content -Path "log.txt" -Value "==============================`n"
-# }
+foreach ($data in $dataset) {
+    # Add-Content -Path "log.txt" -Value "======== dataset: $data ========`n"
+    # foreach ($rf in $tf) {
+    #     foreach ($bias in $tf) {
+            # Add-Content -Path "log.txt" -Value "---- rf $rf - bias $bias ----`n"
+            # D:/anaconda3/envs/pytorch/python.exe ./example.py --dataset $data
+            D:/anaconda3/envs/pytorch/python.exe ./main.py --dataset $data
+    #     }
+    # }
+    # Add-Content -Path "log.txt" -Value "==============================`n"
+}
 
-D:/anaconda3/envs/pytorch/python.exe ./main.py --dataset ../UJIndoorLoc --pretrain False 
-D:/anaconda3/envs/pytorch/python.exe ./example.py --dataset ../UJIndoorLoc
+# D:/anaconda3/envs/pytorch/python.exe ./main.py --dataset ../UJIndoorLoc --pretrain False 
+# D:/anaconda3/envs/pytorch/python.exe ./example.py --dataset ../UJIndoorLoc
